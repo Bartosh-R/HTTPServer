26f92b90516b4b1ec23a4aa57394739a08a9d964
Bartosz Rumiński <bartoshx@gmail.com>
2016-02-07 18:25:46 +0100

    Deleted comments

--- makefile
+++ makefile
@@ -1,5 +1,5 @@
 all:
-	gcc -g -Wall -Wextra server.c
+	gcc -Wall -Wextra server.c
 term:
 	telnet -4 localhost 9080
 kill:
--- server.c
+++ server.c
@@ -1,51 +1,51 @@
 #include <stdio.h>
 #include <sys/types.h>
 #include <sys/socket.h>
-#include <string.h>	// memset()
-#include <arpa/inet.h> // htonl()
+#include <string.h>
+#include <arpa/inet.h>
 #include <unistd.h>
-#include <stdlib.h> // calloc 
-#include <fcntl.h>  // dla czytania z pliku 
+#include <stdlib.h>
+#include <fcntl.h>
 #include <stdio.h>
-#include <signal.h>	// do obsługi sygnałów
+#include <signal.h>
 
-#define SIZE 1024	// podstawowy rozmiar stringa
-#define BUFF_SIZE 1024	// podstawowy rozmiar bufora
+#define SIZE 1024
+#define BUFF_SIZE 1024
 
 
-int checkCommand(char * header, char * path);	// sprawdza otrzymaną komendę, gdy jest prawidłowa zwraca 1, w przeciwnym 0
-int sendFile(char *path);	// czyta plik podany w path i wysyła do klienta
+int checkCommand(char * header, char * path);
+int sendFile(char *path);
 
-void onRespond(char * request);	// funkcja obsługująca żądanie kilenta
-void onInterrupt(int signal); // funkcja wywoływana po otrzymaniu sygnału
+void onRespond(char * request);
+void onInterrupt(int signal);
 
-int getExtension(char * path); // podaje numer indeksu odpoowiedni dla wysyłanego rodzaju pliku
-long getFileSize(char *path); // zwraca rozmiar pliku w bajtach
+int getExtension(char * path);
+long getFileSize(char *path);
 
-int sockfd;	// deskryptor gniazda
-int polfd;	// deskryptor zaakceptowanego gniazda
+int sockfd;
+int polfd;
 
-int childpid;	// przechowuje pid pozyskany z forka
+int childpid;
 
-// przechowuje znane rozszerzenia plików
+
 char * extensions[] = {
 	".gif", ".jpg",".jpeg" ".png", ".zip",
  ".gz", ".tar", ".htm", ".html"};
- 
-// przechowuje odpowiadające 
+
+
 char * filetypes[] = {
  	"image/gif", "image/jpeg","image/jpeg", "image/png", "image/zip",
   "image/gz", "image/tar", "text/html", "text/html"};
 
 
-// szablon nagłówka odpowiedzi pomyślnej
+
 const char HEAD[] = "HTTP/1.0 200 OK\n\
 	Server: NAZWA_SERWERA\n\
 	Content-Length: %ld\
 	\nConnection: close\
 	\nContent-Type: %s\n\n";
 
-// szablony nagłówka odpowiedzi niepomyślnej
+
 const char ERROR_HEAD[] = "HTTP/1.1 403 Forbidden\n\
 	Server: NAZWA_SERWERA\n\
 	Content-Length:7\
@@ -55,45 +55,41 @@ const char ERROR_HEAD[] = "HTTP/1.1 403 Forbidden\n\
 
 
 int main(void){
-    struct sockaddr_in server_addr;	// adres serwera 
-	
-	char request[SIZE];	// przechowuje odebranie żądanie
-	char buff[255]; 
-	int rozmiar;
 
-    memset(&server_addr, 0, sizeof(server_addr)); // "zerowanie" struktury sockaddr_in
-    server_addr.sin_family = AF_INET; // IPv4 
-    server_addr.sin_addr.s_addr = htonl(INADDR_ANY); // slucham na wszystkich interfejsach 
-	server_addr.sin_port = htons(9080);	// slucham na porcie 9080 
-    
-    
-    sockfd = socket(AF_INET, SOCK_STREAM, 0); // tworze gniazdo - na razie nie zwiazane z zadnym portem/adresem 
-    bind(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr)); // "podpinam" gniazdo pod konkretny port/adres 
-	listen(sockfd, SOMAXCONN); // próbuje nawiązać połączenie z serwerem
-	
-	struct sigaction sa; // tworzenie struktry do obsługi sygnałów
-	sa.sa_handler = onInterrupt; // przypisanie funkcji onInterrupt jako handler (uchwyt)
-	sigemptyset(&sa.sa_mask); // "czyszczenie maski"
-	sa.sa_flags = 0; // ustawienie maski
-	
-	sigaction(SIGINT, &sa, NULL); // ustawienie obsługi Ctrl-C przez funkcję onInterrupt
+  struct sockaddr_in server_addr;
+
+	char request[SIZE];
+
+  memset(&server_addr, 0, sizeof(server_addr));
+  server_addr.sin_family = AF_INET;
+  server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	server_addr.sin_port = htons(9080);
+
+
+  sockfd = socket(AF_INET, SOCK_STREAM, 0);
+  bind(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr));
+	listen(sockfd, SOMAXCONN);
+
+	struct sigaction sa;
+	sa.sa_handler = onInterrupt;
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = 0;
+
+	sigaction(SIGINT, &sa, NULL);
 
 
     while(1){
-		polfd = accept(sockfd, NULL, NULL);	// akcepotowanie połączenia i uzyskanie deskryptora
-		memset(request, 0, sizeof(request));
-		while((rozmiar = read(polfd, buff, 255)) != 0) {
-			write(1, buff, rozmiar);
-		}
-		write(1, request, strlen(request)); // wypisanie otrzymanego żądania w konsoli
+		polfd = accept(sockfd, NULL, NULL);
+    read(polfd, request, 1024);
+		write(1, request, strlen(request));
 		 if((childpid =fork()) == 0) {
-			onRespond(request);	// stworzenie potomka który obsłuży żądanie
-			close(polfd); // zamykanie deskryptora połączenia w potomku
-			close(sockfd); // zamykanie socketu
-			exit(0); // zakończenie procesu po wykonaniu zadania
+			onRespond(request);
+			close(polfd);
+			close(sockfd);
+			exit(0);
 		} else {
-			close(polfd); // zamknięcie nie potrzebnego deskryptora
-			// tutaj nic nie robi;
+			close(polfd);
+
 		}
     }
 
@@ -101,16 +97,16 @@ int main(void){
 }
 
 void onRespond(char * request) {
-	char respond[SIZE];	// przechowuje całą odpowiedź serwera łącznie z nagłówkiem
-	char *path; // scieżka z żądania http
-	
-	path = calloc(SIZE, sizeof(char)); // wyświetlanie zapytań klienta na ekranie
-	if(checkCommand(request, path) == 1) { // sprawdza poprawność otrzymanego żądanie (1 - poprawne, 0 - niepoprawne)
-		long fileSize; // przechowuje rozmiar pliku 
-		if((fileSize = getFileSize(path)) != -1) { // pobranie rozmiaru pliku i sprawdzenie czy istnieje ( -1 świady o braku możliwości otworzenia)
-			int type = getExtension(path); // pobranie indeksu tablicy oznaczonego 
+	char respond[SIZE];
+	char *path;
+
+	path = calloc(SIZE, sizeof(char));
+	if(checkCommand(request, path) == 1) {
+		long fileSize;
+		if((fileSize = getFileSize(path)) != -1) {
+			int type = getExtension(path);
 			sprintf(respond, HEAD, fileSize, filetypes[type]);
-			write(polfd, respond, strlen(respond)); // 
+			write(polfd, respond, strlen(respond));
 			sendFile(path);
 		} else {
 			write(polfd, ERROR_HEAD, strlen(ERROR_HEAD));
@@ -124,84 +120,80 @@ int getExtension(char * path){
 	while(*path != '\0' && *path != '.') {
 		path++;
 	}
-	
+
 	int i;
 	for(i = 0; i<9; i++) {
 		if(strcmp(path, extensions[i]) == 0){
 			return i;
 		}
 	}
-	
-	return 8; // w przypadku nie znanego rozszerzenia oznacza jako text/html
+
+	return 8;
 }
 
 
 
 void onInterrupt(int signal){
-	(void) signal; // aby pozbyć się ostrzeżenia o nieużywanej zmiennej
-	close(sockfd);	// zamknięcie socketu
-	kill(childpid, SIGTERM); // zabicie procesu potomnego jeżeli nie zrobił tego sam
-	exit(0); // wyjście z aplikacji
+	(void) signal;
+	close(sockfd);
+	kill(childpid, SIGTERM);
+	exit(0);
 }
 
 int checkCommand(char * header, char * path){
-	char * pch; // zmienne przechowująca aktualny fragment analizowanego nagłówka
-	
-	strcat(path,"."); // katalog root dla serwera 
-		
-    pch = strtok (header," "); // sprawdzenie czy żądanie zaczyna się od słowa GET
-	if(strcmp(pch, "GET") != 0)
-		return 0;
-		
-	pch = strtok (NULL, " "); // pobranie i zapisanie ścieżki z żądania
+	char * pch;
+
+	strcat(path,".");
+
+    pch = strtok (header," ");
+	// if(strcmp(pch, "GET") != 0)
+	// 	return 0;
+
+	pch = strtok (NULL, " ");
 	strcat(path,pch);
-	
-	if(strcmp(path, "./") == 0) // ustawienie index.html jako domyślnego pliku przy zapytaniach
+
+	if(strcmp(path, "./") == 0)
 		strcat(path, "index.html");
-	
-	pch = strtok (NULL, " \r\n"); // sprawdzenie czy żadanie kończy się HTTP/1.0 lub HTTP/1.1
+
+	pch = strtok (NULL, " \r\n");
 	if(strcmp(pch, "HTTP/1.1") == 0 ||
 		 strcmp(pch, "HTTP/1.0") == 0 )
 		return 1;
-		
-	return 0; // zwraca 0 gdy żądanie nie posiada HTTP/1.0 lub HTTP/1.1
+
+	return 0;
 }
 
 int sendFile(char *path) {
-	char buff[BUFF_SIZE]; // buffor do którego zapisywane są dane z pliku
-	int readed;	// liczba przecztanych znaków
-	
-	int fd = open(path, O_RDONLY); // otwiera plik tylko do odczytu
-	
-	if (fd < 0) 
-		return -1; // zwraca -1 gdy nie udało się otworzyć pliku	
-		
+	char buff[BUFF_SIZE];
+	int readed;
+
+	int fd = open(path, O_RDONLY);
+
+	if (fd < 0)
+		return -1;
+
 	while((readed = read(fd, buff, BUFF_SIZE)) != 0) {
-		write(polfd, buff, readed);	// wysyłanie danych do klienta
-		memset(buff,0,strlen(buff)); // czyszcenie bufora przed ponownym użyciem
+		write(polfd, buff, readed);
+		memset(buff,0,strlen(buff));
 	}
-		
-	close(fd); // zamkykanie deskryptora
+
+	close(fd);
 	return 0;
 }
 
 long getFileSize(char *path){
-	int result = 0;	// całkowita liczba przecztanych znaków
-	int readed;	// liczba przecztanych znaków
-	
-	char buff[BUFF_SIZE]; // buffor do czytania pliku;
-	
-	int fd = open(path, O_RDONLY); // otwiera plik tylko do odczyt
-	if (fd < 0) 
-		return -1; // zwraca -1 gdy nie udało się otworzyć pliku	
-		
+	int result = 0;
+	int readed;
+
+	char buff[BUFF_SIZE];
+
+	int fd = open(path, O_RDONLY);
+	if (fd < 0)
+		return -1;
+
 	while((readed = read(fd, buff, BUFF_SIZE)) != 0) {
-		result += readed; // dodawanie przeczytanych danych do zmiennej przechowującej ogólny rozmiar
+		result += readed;
 	}
 
 	return result;
 }
-
-
-
-
